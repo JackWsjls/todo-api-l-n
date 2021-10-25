ff898b9e53ca9c026415bd8d366fd3e409eac5c3
Wsjls <418383219@qq.com>
2021-10-26 01:29:17 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,5 +12,15 @@ module.exports = {
     env_production: {
       NODE_ENV: "production"
     }
-  }]
+  }],
+  deploy: {
+   production : {
+    user: 'root',           //ssh 用户
+    host: '123.56.185.180',       //ssh 地址
+    ref: 'origin/feature01',       //GIT远程/分支
+    repo: 'git@github.com:JackWsjls/todo-api-l-n.git',  //git地址
+    path: '/var/www/production',    //服务器文件路径
+    "post-deploy": 'npm install && pm2 reload ecosystem.config.js --env production' //部署后的动作
+   }
+ }
 };
